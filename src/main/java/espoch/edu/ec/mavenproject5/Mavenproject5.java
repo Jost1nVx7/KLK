0d3bec40f19a40e5c369609164c9b502f4df7fa2
Jost1nVx7 <jostinjd2002@gmail.com>
2024-10-30 15:26:32 -0500

    KLK

---
+++ src/main/java/espoch/edu/ec/mavenproject5/Mavenproject5.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package espoch.edu.ec.mavenproject5;
+
+/**
+ *
+ * @author Global Technology Ec
+ */
+public class Mavenproject5 {
+
+    public static void main(String[] args) {
+        System.out.println("SUMAR Y RESTAR");
+    }
+}
